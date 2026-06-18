72c00c6523cd665c0cd2387e098a06d38572ff42
AlePan <darenagel@hotmail.it>
2026-06-18 06:33:15 +0200

    Refactor ThemeService and AppComponent to use inject for ThemeService; improve theme color model formatting

--- practice/10. tech-blog-theming/src/app/services/theme.service.ts
+++ practice/10. tech-blog-theming/src/app/services/theme.service.ts
@@ -1,11 +1,13 @@
-import { computed, Injectable, signal } from '@angular/core';
+import { computed, effect, inject, Injectable, signal } from '@angular/core';
 import { THEME_SCHEME } from '../models/theme-scheme.model';
 import { THEME_COLORS } from '../models/theme-colors.model';
+import { DOCUMENT } from '@angular/common';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ThemeService {
+  readonly #document = inject(DOCUMENT);
 
   readonly possibleColors = THEME_COLORS;
   readonly possibleSchemes = THEME_SCHEME;
@@ -21,18 +23,30 @@ export class ThemeService {
   readonly #scheme = computed(() => this.selectedScheme().value);
 
   setColor(name: string) {
-    const item = this.possibleColors.find(item => item.name === name);
+    const item = this.possibleColors.find((item) => item.name === name);
     if (item) {
       this.#selectedColor.set(item);
     }
   }
 
   setScheme(name: string) {
-    const item = this.possibleSchemes.find(item => item.name === name);
+    const item = this.possibleSchemes.find((item) => item.name === name);
     if (item) {
       this.#selectedScheme.set(item);
     }
   }
 
-  constructor() { }
+  constructor() {
+    effect(() => {
+      this.#document.body.style.setProperty('--theme-primary', this.#primary());
+    });
+
+    effect(() => {
+      this.#document.body.style.setProperty('--theme-accent', this.#accent());
+    });
+
+    effect(() => {
+      this.#document.body.style.setProperty('--color-scheme', this.#scheme());
+    });
+  }
 }
